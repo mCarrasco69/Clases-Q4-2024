68002703ee28d22d8813a6542b0a5973ba9fd32b
J Miguel MC <162650216+mCarrasco69@users.noreply.github.com>
2024-10-16 16:56:16 -0600

    Crear clase ave

---
+++ Clases Q4 2024/Ave.cs
@@ -0,0 +1,17 @@
+﻿
+
+using System.Drawing;
+
+namespace Clases_Q4_2024
+{
+   public class Ave
+    {
+
+        public string Nombre { get; set; }
+        public string Especie { get; set; }
+        public string Color { get; set; }
+        public double Tamano { get; set; }
+        public string Habitat { get; set; }
+   
+    }
+}
